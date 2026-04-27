cd8f0787283564374ba3b2fcc1fe1f4ed2218720
0802222 <0802222@naver.com>
2026-04-28 02:37:42 +0900

    initial commit

---
+++ src/main/java/com/cho/business/BusinessSupportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cho.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessSupportSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessSupportSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cho/business/BusinessSupportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cho.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessSupportSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
